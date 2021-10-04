350b907ba19573d6f311bbfd27cf62c703b34e41
Nnadozie Opara <nnadozieopara@yahoo.com>
2021-10-04 16:22:01 +0100

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContententLoaded", (event)=>{
+    let topButton = document.getElementById("topButton");
+
+    window.onscroll = () =>{
+        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+            topButton.style.display = "block"
+        }
+        else{
+            topButton.style.display = "none"
+        }
+    }
+})
